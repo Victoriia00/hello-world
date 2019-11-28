5519db2ad6737455a93435877c993d8e58e925e6
Victoriia00 <55548301+Victoriia00@users.noreply.github.com>
2019-11-28 22:25:29 +0300

    Create Graphics_meneger.h

---
+++ Graphics_meneger.h
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+class Basic_object
+{
+    public:
+    //virtual ~Basic_object(){}
+    virtual void Draw(sf::RenderWindow* window){}
+};
+
+
+
+
+class Graphic_Manager
+{
+    private:
+        Basic_object** objects = new Basic_object*[len];
+        int len = 100;
+        int pos = 0; 
+
+    public:
+            /*
+            ~Graphic_Manager()
+            {
+                for(int i = 0; i < pos; i++){}
+                delete[] objects;
+            }
+            */
+        void drawAll(sf::RenderWindow* window)
+        {
+            for(int i = 0; i < pos ; i++)
+            {
+                objects[i]->Draw(window);
+            }
+        }
+
+        void registrate(Basic_object* obj)
+        {
+            if(pos > len - 1)
+            {
+                this->len = this->len * 2;
+                objects = (Basic_object**) realloc(objects, this->len * sizeof(Basic_object*));
+            }
+            objects[this->pos] = obj;
+            this->pos += 1;
+            
+        }
+
+        void unregistrate(Basic_object* obj)
+        {
+            for(int i = 0; i < pos - 1; i++)
+                if(objects[i] == obj)
+                {
+                    for(int j = i; j < pos - 1; j++)
+                         objects[j] = objects[j+1];
+                     objects[pos - 1] = NULL;
+                     return;
+                }
+        }
+
+       
+};
