43350b0f48226c23733cff6b804255d2c3ed4be4
Victoriia00 <55548301+Victoriia00@users.noreply.github.com>
2019-12-19 17:50:14 +0300

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,57 +1,157 @@
 #include <iostream>
 
-class Node 
+class Node
 {
 public:
-	Node() = default;
-	Node(int d) : data(d)
-	{}
+	Node() {};
+	Node(int d, Node* n)
+	{
+		data = d;
+		next = n;
+	}
 
 	int data;
-	Node* next = nullptr;
+	Node* next = NULL;
 };
 
 class List
 {
 public:
-	List() : first(nullptr) 
+	List() 
 	{}
 
-	void append(int d);
-	void remove(int n);
+	~List()
+	{
+		while (size!=0)
+		{
+			popfront();
+		}
+	}
 
-	Node* first;
+	int length();
+	void pushfront(int d);
+	int popfront();
+	void pushback(int d);
+	int popback();
+	void insert(int d, int index);
+	void remove(int index);
+
+	Node* first = NULL;
+	int size = 0;
 };
 
 
 int main()
 {
 	List l;
-	l.append(5);
+	l.pushback(100);
+	l.pushback(99);
+	l.insert(98,2);
+	l.remove(2);
+	//std::cout << l.popback() << " "<< l.size;
 }
 
-void List::append(int d)
+int List::length()
 {
+	return this->size;
+}
+
+void List::pushfront(int d)
+{
+	first = new Node(d, first);
+	++size;
+}
+
+int List::popfront()
+{
+	if (first == NULL)
+	{
+		return NULL;
+	}
+
+	int popped = first->data; 
+	Node* temp = first;
+	first = first->next;
+	delete temp;
+	--size;
+	return popped;
+}
+
+void List::pushback(int d)
+{
+	Node* newNode = new Node(d, NULL);
 	Node* current = first;
-	while (current->next)
+
+	if (current == NULL)
+	{
+		first = newNode;
+		++size;
+		return;
+	}
+
+	while (current->next != NULL)
 	{
 		current = current->next;
 	}
 
-	current->next = &(Node(d));
+	current->next = newNode;
+	++size;
 }
-void List::remove(int n)
-{
-	Node* current = first;
 
-	for (int i = 1; i < n;++i)
+int List::popback()
+{
+	if (first == NULL)
+	{
+		return NULL;
+	}
+
+	Node* current = first;
+	while (current->next != NULL)
 	{
 		current = current->next;
 	}
 
-	Node* removed = current->next;
+	int popped = current->data;
+	Node* temp = current;
+	current = NULL;
+	--size;
+	delete temp;
+	return popped;
+}
 
-	current->next = removed->next;
+void List::insert(int d, int index)
+{
+	if (index > size)
+	{
+		return;
+	}
+	Node* current = first;
+	for (int i = 1; i < index -1; ++i)
+	{
+		current = current->next;
+	}
 
-	removed->next = nullptr;
+	Node* newNode = new Node(d, current->next);
+	current->next = newNode;
+	++size;
+}
+
+void List::remove(int index)
+{
+	if (index > size)
+	{
+		return;
+	}
+
+
+	Node* current = first;
+	for (int i = 1; i < index-1; ++i)
+	{
+		current = current->next;
+	}
+
+	Node* temp = current->next;
+	current->next = temp->next;
+	--size;
+	delete temp;
 }
